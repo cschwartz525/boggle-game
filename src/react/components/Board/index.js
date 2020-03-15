5b38780367a9a848fcce762b65bcd18e149bf055
Craig Schwartz <craigschwartz525@gmail.com>
2020-03-15 15:47:56 -0400

    Load board from START_GAME action

--- src/react/components/Board/index.js
+++ src/react/components/Board/index.js
@@ -2,24 +2,14 @@ import { connect } from 'react-redux';
 import { startGame } from '../../redux/actions';
 import Board from './component';
 
-const mapStateToProps = () => (
-    {
-        // TODO: Remvoe mock data, bind to store
-        rows: [
-            ['A', 'B', 'C', 'D'],
-            ['E', 'F', 'G', 'H'],
-            ['I', 'J', 'K', 'L'],
-            ['M', 'N', 'O', 'P']
-        ]
-    }
-);
+const mapStateToProps = (state) => ({
+    rows: state.game.board
+});
 
-const mapDispatchToProps = (dispatch) => (
-    {
-        startGame: () => {
-            dispatch(startGame());
-        }
+const mapDispatchToProps = (dispatch) => ({
+    startGame: () => {
+        dispatch(startGame());
     }
-);
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Board);
--- src/react/redux/reducers/game.js
+++ src/react/redux/reducers/game.js
@@ -1,6 +1,14 @@
+import ActionStatus from '../actions/status';
+
 export default (state = {}, action) => {
     switch (action.type) {
     case 'START_GAME':
+        if (action.status === ActionStatus.SUCCESS) {
+            return {
+                ...state,
+                board: action.payload ? action.payload.board : [[]]
+            };
+        }
         return state;
     default:
         return state;
